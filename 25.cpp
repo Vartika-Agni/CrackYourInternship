ecffc78f395cb5d8aceb8e2db154cb661066c1f4
Vartika-Agni <98183015+Vartika-Agni@users.noreply.github.com>
2022-06-10 23:06:45 +0530

    Add files via upload

---
+++ 25.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+         ListNode* current = head;
+         ListNode *prev = NULL, *next = NULL;
+ 
+        while (current != NULL) {
+            // Store next
+            next = current->next;
+            // Reverse current node's pointer
+            current->next = prev;
+            // Move pointers one position ahead.
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        return head;
+    }
+};
